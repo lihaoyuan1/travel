5d23219a62494eef757edd5bd79fab0ea2bb5000
lihaoyuan1 <1617721824@qq.com>
2019-01-22 22:09:52 +0800

    add

--- travel/src/pages/city/city.vue
+++ travel/src/pages/city/city.vue
@@ -2,8 +2,8 @@
   <div>
     <city-header></city-header>
     <search></search>
-    <list></list>
-    <alphabet></alphabet>
+    <list :cities="cities" :hot="hotCities"></list>
+    <alphabet :cities="cities"></alphabet>
   </div>
 </template>
 
@@ -12,6 +12,7 @@ import CityHeader from './components/header/header'
 import Search from './components/search/search'
 import List from './components/list/list'
 import Alphabet from './components/alphabet/alphabet'
+import axios from 'axios'
 export default {
   name: 'city',
   components: {
@@ -19,6 +20,27 @@ export default {
     Search,
     List,
     Alphabet
+  },
+  data () {
+    return {
+      cities: {},
+      hotCities: []
+    }
+  },
+  methods: {
+    getCityInfo () {
+      axios.get('/api/city.json').then(response => {
+        response = response.data
+        if (response.ret && response.data) {
+          const data = response.data
+          this.cities = data.cities
+          this.hotCities = data.hotCities
+        }
+      })
+    }
+  },
+  created () {
+    this.getCityInfo()
   }
 }
 </script>
--- travel/src/pages/city/components/alphabet/alphabet.vue
+++ travel/src/pages/city/components/alphabet/alphabet.vue
@@ -1,18 +1,15 @@
 <template>
   <ul class="list">
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
-    <li class="item">A</li>
+    <li class="item" v-for="(item, key) in cities" :key="key">{{key}}</li>
   </ul>
 </template>
 
 <script>
 export default {
-  name: 'alphabet'
+  name: 'alphabet',
+  props: {
+    cities: Object
+  }
 }
 </script>
 
