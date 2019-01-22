1753284b3da97cdf93b88d7fb46f93419454c71b
lihaoyuan1 <1617721824@qq.com>
2019-01-22 19:56:22 +0800

    add search

--- travel/src/pages/city/city.vue
+++ travel/src/pages/city/city.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <city-header></city-header>
+    <search></search>
   </div>
 </template>
 
 <script>
 import CityHeader from './components/header/header'
+import Search from './components/search/search'
 export default {
   name: 'city',
   components: {
-    CityHeader
+    CityHeader,
+    Search
   }
 }
 </script>
---
+++ travel/src/pages/city/components/search/search.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="输入城市名或拼音"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'search'
+}
+</script>
+
+<style scoped lang="stylus">
+  @import "search.styl"
+</style>
